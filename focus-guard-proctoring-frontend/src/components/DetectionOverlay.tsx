2d8c0c3272909d246c74ee403e28f432e0837466
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2025-09-15 12:46:57 +0530

    fixed detection overlay, lower video quality

--- focus-guard-proctoring-frontend/src/components/DetectionOverlay.tsx
+++ focus-guard-proctoring-frontend/src/components/DetectionOverlay.tsx
@@ -1,5 +1,6 @@
-import type { FocusMetrics } from '@/types/detection';
-import React from 'react';
+import type { FocusMetrics } from "@/types/detection";
+import React, { useState } from "react";
+import { X, MonitorCog   } from "lucide-react";
 
 interface DetectionOverlayProps {
   predictions: any[];
@@ -9,50 +10,79 @@ interface DetectionOverlayProps {
 const DetectionOverlay: React.FC<DetectionOverlayProps> = ({
   focusMetrics,
 }) => {
-  
+  const [showDetails, setShowDetails] = useState(true);
 
   const getScoreColor = (score: number) => {
-    if (score >= 80) return 'text-green-500';
-    if (score >= 60) return 'text-yellow-500';
-    return 'text-red-500';
+    if (score >= 80) return "text-green-500";
+    if (score >= 60) return "text-yellow-500";
+    return "text-red-500";
+  };
+
+  const handleClick = () => {
+    setShowDetails((prev) => !prev);
   };
 
   return (
-    <div className="absolute inset-0 pointer-events-none">
+    <div className="absolute inset-0 ">
       {/* Focus Metrics Overlay */}
-      <div className="absolute top-4 right-4 bg-black bg-opacity-80 rounded-lg p-4 text-white min-w-48">
-        <h3 className="font-semibold mb-2 text-sm">Focus Analysis</h3>
-        <div className="space-y-1 text-xs">
-          <div className="flex justify-between">
-            <span>Focus Score:</span>
-            <span className={`font-medium ${getScoreColor(focusMetrics.focusScore)}`}>
-              {focusMetrics.focusScore}%
-            </span>
+
+      {showDetails ? (
+        <div className="absolute top-4 right-4 bg-black/40 bg-opacity-80 rounded-lg p-4 text-white min-w-48">
+          <div className="flex justify-between items-center">
+            <h3 className="font-semibold mb-2 text-sm">Focus Analysis</h3>
+            <button onClick={handleClick}>
+              <X />
+            </button>
           </div>
-          <div className="flex justify-between">
-            <span>Eye Contact:</span>
-            <span className={`font-medium ${getScoreColor(focusMetrics.eyeContactScore)}`}>
-              {focusMetrics.eyeContactScore}%
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <span>Head Pose:</span>
-            <span className={`font-medium ${getScoreColor(focusMetrics.headPoseScore)}`}>
-              {focusMetrics.headPoseScore}%
-            </span>
-          </div>
-          <div className="border-t border-gray-600 pt-1 mt-2">
-            <div className="flex justify-between font-semibold">
-              <span>Overall:</span>
-              <span className={getScoreColor(focusMetrics.overallAttention)}>
-                {focusMetrics.overallAttention}%
+
+          <div className="space-y-1 text-xs">
+            <div className="flex justify-between">
+              <span>Focus Score:</span>
+              <span
+                className={`font-medium ${getScoreColor(
+                  focusMetrics.focusScore
+                )}`}
+              >
+                {focusMetrics.focusScore}%
               </span>
+            </div>
+            <div className="flex justify-between">
+              <span>Eye Contact:</span>
+              <span
+                className={`font-medium ${getScoreColor(
+                  focusMetrics.eyeContactScore
+                )}`}
+              >
+                {focusMetrics.eyeContactScore}%
+              </span>
+            </div>
+            <div className="flex justify-between">
+              <span>Head Pose:</span>
+              <span
+                className={`font-medium ${getScoreColor(
+                  focusMetrics.headPoseScore
+                )}`}
+              >
+                {focusMetrics.headPoseScore}%
+              </span>
+            </div>
+            <div className="border-t border-gray-600 pt-1 mt-2">
+              <div className="flex justify-between font-semibold">
+                <span>Overall:</span>
+                <span className={getScoreColor(focusMetrics.overallAttention)}>
+                  {focusMetrics.overallAttention}%
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      
+      ) : (
+        <div className=" absolute top-4 right-4 bg-black/40 bg-opacity-80 rounded-lg p-2 text-white">
+          <button onClick={handleClick}>
+            <MonitorCog  />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
